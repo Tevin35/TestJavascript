1f179d7ea1ac7007b79eae34393a559b1143bbe8
sonic-san <tevin.abhay@gmail.com>
2022-05-02 11:31:56 +0200

    ajout de salut.js

--- salut.js
+++ salut.js
@@ -1,1 +1,17 @@
 console.log("Hello JS")
+
+/*
+Déclaration d'une variable
+ */
+
+let aNumber = 12;
+
+let result = multiply(aNumber);
+
+//déclaration d'une fonction
+// mot clé function
+function multiply(number) {
+    return number * 2;
+}
+
+console.log("Le double de " + aNumber + " est " + result);
