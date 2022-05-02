72a67e06d88f470bb7d465e7a6c41a69a087e267
sonic-san <tevin.abhay@gmail.com>
2022-05-02 12:23:21 +0200

    compare des voyelles avec la phrase Une phrase qui contient des lettres"

--- demoStructure.js
+++ demoStructure.js
@@ -1,5 +1,12 @@
 let sentence = "Une phrase qui contient des lettres";
 
 function isVowel(letter){
-    if(letter != undefined && "aeuioy".indexOf())
+    if(letter != undefined && "aeuioy".indexOf(letter) >=0){
+        return true;
+    }
+    return false;
+}
+
+for(let letter of sentence){
+    console.log(letter + " => " + isVowel(letter.toLowerCase()));
 }
